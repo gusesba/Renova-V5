79656367b0d31d6ce53dbd9321a0e411cb1be318
gustavo.bareta <gustavo.bareta@mps.com.br>
2024-06-12 10:03:26 -0300

    routing

---
+++ src/app/(mainLayout)/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import "@/css/satoshi.css";
+import "@/css/style.css";
+import React, { useState } from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        <main>
+          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
