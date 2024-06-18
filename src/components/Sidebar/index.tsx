0eff6523d846d45251b5d9a8e45db2c025a1ef7c
gustavo.bareta <gustavo.bareta@mps.com.br>
2024-06-18 11:54:09 -0300

    sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -59,7 +59,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   <li>
                     <Link
                       href="/clientes"
-                      className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                      className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:text-white ${
                         pathname === "/clientes" && "text-white"
                       }`}
                     >
@@ -69,7 +69,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   <li>
                     <Link
                       href="/clientes/novo"
-                      className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+                      className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:text-white ${
                         pathname === "/clientes/novo" && "text-white"
                       }`}
                     >
